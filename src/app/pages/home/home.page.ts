a9e2147592fe227b2ec239a3ae54b82f61a40108
alejandrossm <alejandrossm@outlook.com>
2024-10-01 16:59:09 -0300

    Datos en home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/services/datos.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private datossrv: DatosService) { }
+
+  ngOnInit() {
+    this.datossrv.getCategorias().subscribe(data => {
+      console.log(data);
+    })
+  }
 
 }
--- src/app/services/datos.service.ts
+++ src/app/services/datos.service.ts
@@ -7,4 +7,9 @@ import { Injectable } from '@angular/core';
 export class DatosService {
 
   constructor(private httpclient:HttpClient) { }
+
+  getCategorias(){
+    return this.httpclient.get(`https://www.themealdb.com/api/json/v1/1/categories.php`);
+  }
+
 }
