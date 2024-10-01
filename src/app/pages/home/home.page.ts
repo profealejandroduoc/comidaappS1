32be7782703fd0aadcaabafd910b8e49e300fa87
alejandrossm <alejandrossm@outlook.com>
2024-10-01 17:17:41 -0300

    Servicio ok

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/interfaces/icomidas';
 import { DatosService } from 'src/app/services/datos.service';
+
 
 @Component({
   selector: 'app-home',
@@ -8,11 +10,16 @@ import { DatosService } from 'src/app/services/datos.service';
 })
 export class HomePage implements OnInit {
 
+  lista:Categoria[]=[];
+  
   constructor(private datossrv: DatosService) { }
 
   ngOnInit() {
     this.datossrv.getCategorias().subscribe(data => {
       console.log(data);
+      this.lista.push(...data.categories)
+      console.log("MI LISTA************");
+      console.log(this.lista);
     })
   }
 
--- src/app/services/datos.service.ts
+++ src/app/services/datos.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Categorias } from '../interfaces/icomidas';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class DatosService {
   constructor(private httpclient:HttpClient) { }
 
   getCategorias(){
-    return this.httpclient.get(`https://www.themealdb.com/api/json/v1/1/categories.php`);
+    return this.httpclient.get<Categorias>(`https://www.themealdb.com/api/json/v1/1/categories.php`);
   }
 
 }
