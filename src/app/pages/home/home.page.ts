1971980f35c6ddd1a8922f3854c7c187fbadee05
alejandrossm <alejandrossm@outlook.com>
2024-10-29 17:59:07 -0300

    skeleton added

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -12,16 +12,25 @@ import { DatosService } from 'src/app/services/datos.service';
 export class HomePage implements OnInit {
 
   lista:Categoria[]=[];
-  
+  public loaded = false;
   constructor(private datossrv: DatosService, private router:Router) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.sleep(1000);
     this.datossrv.getCategorias().subscribe(data => {
+      
+    
       console.log(data);
       this.lista.push(...data.categories)
       console.log("MI LISTA************");
       console.log(this.lista);
     })
+    this.loaded=true;
+
+  }
+
+  sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   verComidas(tipo_categoria:string){
